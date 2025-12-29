7f702a9e44a45c470b0e221591adbf5e99d41707
15868177836 <15868177836@163.com>
2025-12-29 23:43:30 +0800

    个人主页 笔记目录和内容切换

--- 个人主页/JS/archive.js
+++ 个人主页/JS/archive.js
@@ -2,14 +2,18 @@ export function init() {
   console.log('执行archive.js');
     
   const sideElements = document.querySelectorAll('.side-in');
-  const sideInObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
+  showAllSideIn();
+  function showAllSideIn() {
+    let index = 0;
+    const timer = setInterval(() => {
+      if (index < sideElements.length) {
+        sideElements[index].classList.add('show');
+        index++;
+      } else {
+        clearInterval(timer);
       }
-    });
-  }, { threshold: 0.1 });
-  sideElements.forEach(el => sideInObserver.observe(el));
+    }, 100);
+  }
 
   const activeTagClass = 'activeArchiveTag';
   const tagContainer = document.getElementById('archiveTagContainer');
@@ -19,14 +23,13 @@ export function init() {
     tag.addEventListener('click', handleTagClick);
     if (tag.textContent != '全部') allTag.add(tag.textContent);
   });
-  const showTagSet = new Set(allTag);
+  const showTagSet = new Set();
   function handleTagClick(e) {
     e.preventDefault();
     if (this.textContent == '全部') {
       if (this.classList.contains(activeTagClass)) return;
       console.log(this.textContent);
       showTagSet.clear();
-      allTag.forEach(v => showTagSet.add(v));
       updateTagState();
       updateArticleList();
       return;
@@ -35,18 +38,11 @@ export function init() {
     console.log(this.textContent);
     if (this.classList.contains(activeTagClass)) {
       showTagSet.delete(this.textContent);
-      if (showTagSet.size <= 0) {
-        showTagSet.clear();
-        allTag.forEach(v => showTagSet.add(v));
-      }
       updateTagState();
       updateArticleList();
       return;
     }
 
-    if (showTagSet.size == allTag.size) {
-      showTagSet.clear();
-    }
     showTagSet.add(this.textContent);
     updateTagState();
     updateArticleList();
@@ -54,7 +50,7 @@ export function init() {
 
   function updateTagState() {
     const n = tagList.length;
-    if (showTagSet.size == allTag.size) {
+    if (showTagSet.size == 0) {
       for (let i = 0; i < n; i++) {
         const element = tagList[i];
         if (element.textContent == '全部') {
@@ -72,61 +68,103 @@ export function init() {
         element.classList.remove(activeTagClass);
         continue;
       }
-      if (!element.classList.contains(activeTagClass) && showTagSet.has(element.textContent)) element.classList.add(activeTagClass);
+      if (element.classList.contains(activeTagClass)) {
+        if (!showTagSet.has(element.textContent)) element.classList.remove(activeTagClass);
+      }
+      else if (showTagSet.has(element.textContent)) {
+        element.classList.add(activeTagClass);
+      }
     }
   }
 
-  const articleList = document.querySelectorAll('.article-item')
-  function updateArticleList()
+  let isLoading = false;
+  const articleList = document.querySelectorAll('.article-item');
+  const articleCatalogueContiner = document.getElementById('articleCatalogueContiner');
+  const articleContentContiner = document.getElementById('articleContentContiner');
+  const contentContainer = document.getElementById('articleContent');
+  articleList.forEach(e => e.addEventListener('click', handleArticleLinkClick));
+  async function handleArticleLinkClick(e) {
+    e.preventDefault();
+    if (isLoading) return;
+    loadHTMLContent(this.dataset.src);
+  }
+  async function loadHTMLContent(filePath) {
+    isLoading = true;
+    console.log(filePath);
+    try {
+      const response = await fetch(filePath);
+      if (!response.ok) throw new Error(`请求失败: ${response.status}`);
+      const htmlContent = await response.text();
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(htmlContent, 'text/html');
+      //const container = htmlDoc.getElementById(contentID);
+      //if (!container) throw new Error(`未找到内容: ${contentID}`);
+      contentContainer.innerHTML = htmlDoc.body.innerHTML;//container.innerHTML;
+      articleCatalogueContiner.classList.add('hide');
+      articleContentContiner.classList.add('show');
+    }
+    catch (error) {
+      console.error(`加载${filePath}失败: ${error}`);
+      contentContainer.innerHTML = `<div>加载失败: ${error.message}</div>`;
+    }
+    finally {
+      isLoading = false;
+    }
+  }
+  const returnA = document.getElementById('return');
+  returnA.addEventListener('click', handleReturnClick);
+  async function handleReturnClick(e) {
+    e.preventDefault();
+    if (isLoading) return;
+    isLoading = true;
+    articleCatalogueContiner.classList.remove('hide');
+    articleContentContiner.classList.remove('show');
+    await new Promise(resolve => setTimeout(resolve, 300));
+    contentContainer.innerHTML = '';
+    isLoading = false;
+  }
+
+  const articleCount = document.getElementById('articleCount');
+  articleCount.textContent = articleList.length.toString();
+  async function updateArticleList()
   {
     const sideList = new Set();
     const n = articleList.length;
-    if (showTagSet.size == allTag.size) {
-      for (let i = 0; i < n; i++) {
-        articleList[i].style.display = '';
+    for (let i = 0; i < n; i++) {
+      articleList[i].classList.add('article-item-hidden');
+    }
+    sideElements.forEach(e => {
+      e.classList.remove('show');
+    });
+    await new Promise(resolve => setTimeout(resolve, 100));
+    const tempTag = new Set();
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+      const tags = articleList[i].querySelectorAll('.archiveRoundTag');
+      tempTag.clear();
+      tags.forEach(t => tempTag.add(t.textContent));
+
+      let active = true;
+      showTagSet.forEach(t => {
+        if (!tempTag.has(t)) active = false;
+      });
+      if (active) {
+        articleList[i].classList.remove('article-item-hidden');
         const sideInItem = articleList[i].querySelectorAll('.side-in');
-        sideInItem.forEach(e => {
-          e.classList.add('no-animation');
-          e.classList.remove('show');
-          sideList.add(e);
-        });
-      }
-    } else {
-      for (let i = 0; i < n; i++) {
-        const tags = articleList[i].querySelectorAll('.archiveRoundTag');
-        let active = true;
-        showTagSet.forEach(t => {
-          let hasT = true;
-          tags.forEach(item => {
-            if (item.textContent != t) hasT = false;
-          });
-          if (hasT == false) active = false;
-        });
-        if (active) {
-          articleList[i].style.display = '';
-          const sideInItem = articleList[i].querySelectorAll('.side-in');
-          sideInItem.forEach(e => {
-            e.classList.add('no-animation');
-            e.classList.remove('show');
-            sideList.add(e);
-          });
-        } else {
-          articleList[i].classList.remove('show');
-          articleList[i].style.display = 'none';
-        }
+        sideInItem.forEach(e => sideList.add(e));
+        count++;
       }
     }
-    
+    articleCount.textContent = count.toString();
     let index = 0;
     const list = Array.from(sideList);
     const timer = setInterval(() => {
       if (index < list.length) {
-        list[index].classList.remove('no-animation');
         list[index].classList.add('show');
         index++;
       } else {
         clearInterval(timer);
       }
-    }, 200);
+    }, 100);
   }
 }
